ce2eb6ca22f1c3098965a7fd773dd7cf5bdd5cc2
raistlinthewiz <shalafiraistlin@gmail.com>
2011-11-24 21:31:21 +0200

    Added license file & texts.

--- VoxlrEngine/Universe/World.cs
+++ VoxlrEngine/Universe/World.cs
@@ -1,6 +1,19 @@
-﻿/*    
- * Copyright (C) 2011, Hüseyin Uslu
- *  
+﻿/*
+ * Copyright (C) 2011 - Hüseyin Uslu shalafiraistlin@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 using Microsoft.Xna.Framework;
@@ -52,7 +65,7 @@ namespace VolumetricStudios.VoxlrEngine.Universe
         public int BuildingQueueCount { get { return this.ChunkBuilder.BuildingQueueCount; } }
 
         public BoundingBox BoundingBox { get; set; }       
-        public const byte ViewRange = 1;
+        public const byte ViewRange = 6; // chunk view range.
         
         public World(Game game)
             : base(game)
