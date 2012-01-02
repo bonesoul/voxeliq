e2be3980e8784c8bfc18b05c1afea2e7d889f7f8
raistlinthewiz <shalafiraistlin@gmail.com>
2012-01-02 22:09:55 +0200

    Updated VoxeliqClient2 project, which will be instead using SlimDX!

--- src/v2/VoxeliqClient2/VoxeliqClient2/Program.cs
+++ src/v2/VoxeliqClient2/VoxeliqClient2/Program.cs
@@ -1,21 +1,21 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace VoxeliqClient2
 {
-#if WINDOWS || XBOX
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-            using (Game1 game = new Game1())
-            {
-                game.Run();
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
-#endif
 }
-
--- src/v2/VoxeliqClient2/VoxeliqClient2/Properties/AssemblyInfo.cs
+++ src/v2/VoxeliqClient2/VoxeliqClient2/Properties/AssemblyInfo.cs
@@ -6,23 +6,21 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VoxeliqClient2")]
-[assembly: AssemblyProduct("VoxeliqClient2")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VoxeliqClient2")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("e7a95ca8-49a3-456e-b11d-05cdb1e9bd99")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f9a9b52-b799-4b06-9bdd-a13cd6726460")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
