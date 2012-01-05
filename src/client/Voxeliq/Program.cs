51c96c0aab22304c4cca0b7aff6c2092c5ac0630
raistlinthewiz <shalafiraistlin@gmail.com>
2012-01-05 22:42:30 +0200

    Moved old code to src/old. Created a new project in src/

---
+++ src/client/Voxeliq/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using VolumetricStudios.Voxeliq;
+
+namespace VoxeliqRetake
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
