faded6d5fa4955f2c1b8ddc7b1f91546f0d5257c
raistlinthewiz <shalafiraistlin@gmail.com>
2012-03-19 16:29:33 +0200

    Started implementing tree planting.

--- src/game/Chunks/Generators/Biomes/RainForest.cs
+++ src/game/Chunks/Generators/Biomes/RainForest.cs
@@ -5,6 +5,7 @@
 
 using System;
 using VolumetricStudios.VoxeliqGame.Blocks;
+using VolumetricStudios.VoxeliqGame.Chunks.Generators.Terrain;
 using VolumetricStudios.VoxeliqGame.Utils.Randomization.Procedural;
 
 namespace VolumetricStudios.VoxeliqGame.Chunks.Generators.Biomes
@@ -14,12 +15,48 @@ namespace VolumetricStudios.VoxeliqGame.Chunks.Generators.Biomes
     /// </summary>
     public sealed class RainForest : BiomeGenerator
     {
+        private readonly Random _treePlanter = new Random(TerrainGenerator.DefaultSeed);
+
         public override void ApplyBiome(Chunk chunk, int groundLevel, int groundOffset, int worldPositionX, int worldPositionZ)
         {
+            bool plantTree = _treePlanter.Next(700) == 1;
+
             BlockStorage.Blocks[groundOffset + 1].Type = BlockType.Grass;
 
             if (groundLevel + 1 > chunk.HighestSolidBlockOffset)
                 chunk.HighestSolidBlockOffset = (byte)(groundLevel + 1);
+
+            if (plantTree)
+            {
+                this.PlantTree(chunk, groundLevel + 1, groundOffset + 1, worldPositionX, worldPositionZ);
+            }
         }
+
+        private void PlantTree(Chunk chunk, int grassLevel, int grassOffset, int worldPositionX, int worldPositionZ)
+        {
+            var trunkHeight = (byte) (5 + (byte) _treePlanter.Next(10));
+
+            // trunk
+            for (int y = 1; y <= trunkHeight; y++ )
+            {
+                BlockStorage.Blocks[grassOffset + y].Type = BlockType.Tree;
+            }
+
+            // foliage
+            int radius = 3 + _treePlanter.Next(2);
+
+            for (int i = 0; i < 40 + _treePlanter.Next(4); i++)
+            {
+                int lx = worldPositionX + _treePlanter.Next(radius) - _treePlanter.Next(radius);
+                int ly = grassLevel + trunkHeight + _treePlanter.Next(radius) - _treePlanter.Next(radius);
+                int lz = worldPositionZ + _treePlanter.Next(radius) - _treePlanter.Next(radius);
+
+                // http://techcraft.codeplex.com/SourceControl/changeset/view/5c1888588e5d#TechCraft%2fNewTake%2fNewTake%2fmodel%2fterrain%2fSimpleTerrain.cs
+            }
+
+            if (grassLevel + trunkHeight > chunk.HighestSolidBlockOffset)
+                chunk.HighestSolidBlockOffset = (byte)(grassLevel + trunkHeight);
+
+
     }
 }
