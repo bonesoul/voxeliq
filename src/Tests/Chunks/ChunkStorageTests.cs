6e544745287008c03d7cd365c67911023eaaa930
Hüseyin Uslu <shalafiraistlin@gmail.com>
2012-12-29 13:07:50 +0200

    Added more unit-tests.

--- src/Tests/Chunks/ChunkStorageTests.cs
+++ src/Tests/Chunks/ChunkStorageTests.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using VoxeliqGame;
 using VoxeliqEngine.Chunks;
 using VoxeliqEngine.Common.Vector;
 
-namespace Tests
+namespace Tests.Chunks
 {
     [TestFixture]
     public class ChunkStorageTests
@@ -29,17 +25,14 @@ namespace Tests
         public void TestChunkStorage()
         {
             _chunkStorage[0, 0] = _chunk;
-
-            Assert.IsTrue(_chunkStorage.ContainsKey(0,0));
-            Assert.AreEqual(_chunk, _chunkStorage[0, 0]);
+            Assert.IsTrue(_chunkStorage.ContainsKey(0,0)); // check if we can index back the chunk.
+            Assert.AreEqual(_chunk, _chunkStorage[0, 0]); // check if we can access it back.
 
             _chunkStorage.Remove(0, 0);
-
-            Assert.IsFalse(_chunkStorage.ContainsKey(0, 0));
+            Assert.IsFalse(_chunkStorage.ContainsKey(0, 0)); // check if it's correctly removed.
 
             _chunkStorage[-1, -1] = new Chunk(new Vector2Int(-1, -1));
-
-            Assert.IsTrue(_chunkStorage.ContainsKey(-1, -1));
+            Assert.IsTrue(_chunkStorage.ContainsKey(-1, -1)); // check if we can index negative coordinates for chunks.
         }
     }
 }
