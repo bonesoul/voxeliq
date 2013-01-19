4ed9ae5b8839550c57450f47d30b107f1fda600a
Huseyin Uslu <shalafiraistlin@gmail.com>
2013-01-19 23:23:05 +0000

    moving src/game to src/Game

---
+++ src/Game/Program.cs
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2011 - 2013 Voxeliq Engine - http://www.voxeliq.org - https://github.com/raistlinthewiz/voxeliq
+ *
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the Microsoft Public License (Ms-PL).
+ */
+
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Threading;
+using VoxeliqEngine.Common.Logging;
+using VoxeliqEngine.Common.Versions;
+using VoxeliqEngine.Universe;
+using VoxeliqGame.Settings.Readers;
+
+namespace VoxeliqGame
+{
+    public static class Program
+    {
+        private static readonly Logger Logger = LogManager.CreateLogger(); // logger instance.
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            #if !DEBUG
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler; // Watch for any unhandled exceptions.
+            #endif
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture; // Use invariant culture - we have to set it explicitly for every thread we create to prevent any mpq-reading problems (mostly because of number formats).
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            PrintBanner();
+            PrintLicense();
+            PrintDebugKeys();
+            Console.ResetColor();
+
+            InitLoggers(); // init logging facility.
+
+            Logger.Info("voxeliq v{0} warming-up..", Assembly.GetAssembly(typeof (Player)).GetName().Version);
+            Logger.Info(string.Format("Running over {0} {1}.",VersionInfo.DotNetFramework,VersionInfo.DotNetFrameworkVersion));
+            Logger.Info(string.Format("Using game framework {0} {1}, over {2}.", VersionInfo.GameFramework, VersionInfo.GameFrameworkVersion, VersionInfo.GraphicsApi));            
+
+            using (var game = new SampleGame()) // startup the game.
+            {
+                Logger.Trace("Starting game loop..");
+                game.Run();
+            }
+        }
+
+        #region logging facility
+
+        private static void InitLoggers()
+        {
+            var logSettings = new LogSettings();
+
+            LogManager.Enabled = true; // enable logger by default.
+
+            foreach (var targetConfig in logSettings.Targets)
+            {
+                if (!targetConfig.Enabled)
+                    continue;
+
+                LogTarget target = null;
+                switch (targetConfig.Target.ToLower())
+                {
+                    case "console":
+                        target = new ConsoleTarget(targetConfig.MinimumLevel, targetConfig.MaximumLevel,
+                                                   targetConfig.IncludeTimeStamps);
+                        break;
+                    case "file":
+                        target = new FileTarget(targetConfig.FileName, targetConfig.MinimumLevel,
+                                                targetConfig.MaximumLevel, targetConfig.IncludeTimeStamps,
+                                                targetConfig.ResetOnStartup);
+                        break;
+                }
+
+                if (target != null)
+                    LogManager.AttachLogTarget(target);
+            }
+        }
+
+        #endregion
+
+        #region unhandled exception emitter
+
+        /// <summary>
+        /// Unhandled exception emitter.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+
+            if (e.IsTerminating)
+                Logger.FatalException(ex, "Voxeliq Engine is terminating because of unhandled exception.");
+            else
+                Logger.ErrorException(ex, "Caught unhandled exception.");
+
+            Console.ReadLine();
+        }
+
+        #endregion
+
+        #region console banners
+
+        /// <summary>
+        /// Prints an info banner.
+        /// </summary>
+        private static void PrintBanner()
+        {
+            Console.WriteLine(@"                          .__  .__        ");
+            Console.WriteLine(@" ___  _________  ___ ____ |  | |__| ______");
+            Console.WriteLine(@" \  \/ /  _ \  \/  // __ \|  | |  |/ ____/");
+            Console.WriteLine(@"  \   (  <_> >    <\  ___/|  |_|  < <_|  |");
+            Console.WriteLine(@"   \_/ \____/__/\_ \\___  >____/__|\__   |");
+            Console.WriteLine(@"                  \/    \/            |__|");
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Prints a copyright banner.
+        /// </summary>
+        private static void PrintLicense()
+        {
+            Console.WriteLine("Copyright (C) 2011 - 2013, Voxeliq Engine");
+            Console.WriteLine("mooege comes with ABSOLUTELY NO WARRANTY.");
+            Console.WriteLine("This is free software, and you are welcome to redistribute it under certain conditions; see the LICENSE file for details.");
+            Console.WriteLine();
+        }
+
+        private static void PrintDebugKeys()
+        {
+            Console.WriteLine("Debug keys:");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("F1: Infinitive-world: On/Off.");
+            Console.WriteLine("F2: Fly-mode: On/Off.");
+            Console.WriteLine("F5: Capture Mouse: On/Off.");
+            Console.WriteLine("F10: In-game Debugger: On/Off.");
+            Console.WriteLine("-----------------------------");
+        }
+
+        #endregion
+    }
+}
---
+++ src/Game/SampleGame.cs
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2011 - 2013 Voxeliq Engine - http://www.voxeliq.org - https://github.com/raistlinthewiz/voxeliq
+ *
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the Microsoft Public License (Ms-PL).
+ */
+
+using Microsoft.Xna.Framework;
+using VoxeliqEngine.Common.Logging;
+using VoxeliqEngine.Common.Versions;
+using VoxeliqEngine.Core;
+using VoxeliqEngine.Core.Config;
+using VoxeliqEngine.Debugging.Timing;
+using VoxeliqEngine.Graphics;
+using VoxeliqEngine.Graphics.Effects.PostProcessing.Bloom;
+using VoxeliqGame.Settings.Readers;
+
+namespace VoxeliqGame
+{
+    /// <summary>
+    /// The game client.
+    /// </summary>
+    public class SampleGame : Game
+    {
+        /// <summary>
+        /// Graphics device manager.
+        /// </summary>
+        private readonly GraphicsDeviceManager _graphicsDeviceManager;
+
+        /// <summary>
+        /// Screen manager.
+        /// </summary>
+        public GraphicsManager ScreenManager { get; private set; }
+
+        private TimeRuler _timeRuler;
+
+        private BloomComponent _bloomComponent;
+
+        /// <summary>
+        /// Logging facility.
+        /// </summary>
+        private static readonly Logger Logger = LogManager.CreateLogger();
+
+        /// <summary>
+        /// Creates a new game instance.
+        /// </summary>
+        public SampleGame()
+        {
+            this.Content.RootDirectory = "Content"; // set content root directory.
+            this._graphicsDeviceManager = new GraphicsDeviceManager(this);
+        }
+
+        /// <summary>
+        /// Initializes the game.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Logger.Trace("init()"); // log the init.
+            this.Window.Title = string.Format("Voxeliq [{0}/{1}]", VersionInfo.GameFramework, VersionInfo.GraphicsApi); // set the window title.
+
+            this.IsMouseVisible = false;
+
+            // read settings.
+            var audioSettings = new AudioSettings();
+            var graphicsSettings = new GraphicsSettings();
+
+            // create a new engine configuration.
+            var config = new EngineConfig
+            {
+                Chunk =
+                {
+                    WidthInBlocks = 16,
+                    HeightInBlocks = 128,
+                    LenghtInBlocks = 16,
+                },
+                Cache =
+                {
+                    CacheExtraChunks = true,
+                    ViewRange = 8,
+                    CacheRange = 12,
+                },
+                Graphics =
+                {
+                    Width = graphicsSettings.Width,
+                    Height = graphicsSettings.Height,
+                    FullScreenEnabled = graphicsSettings.FullScreenEnabled,
+                    VerticalSyncEnabled = graphicsSettings.VerticalSyncEnabled,
+                    FixedTimeStepsEnabled = graphicsSettings.FixedTimeStepsEnabled,
+                },
+                World =
+                {
+                    IsInfinitive = true,
+                },
+                Debugging =
+                {
+                    GraphsEnabled = true,
+                },
+                Bloom =
+                {
+                    Enabled = false,
+                    State = BloomState.Default,
+                },
+                Audio =
+                {
+                    Enabled = audioSettings.Enabled,
+                }
+            };
+
+            var engine = new Engine(this, config);
+            this.ScreenManager = new GraphicsManager(this._graphicsDeviceManager, this); // start the screen manager.
+
+            engine.EngineStart += OnEngineStart;
+
+            engine.Run();
+
+            base.Initialize();
+        }
+
+        private void OnEngineStart(object sender, System.EventArgs e)
+        {
+            this._timeRuler = new TimeRuler(this) { Visible = true, ShowLog = true };
+            this.Components.Add(this._timeRuler);
+
+#if XNA
+            this._bloomComponent = new BloomComponent(this);
+            this.Components.Add(this._bloomComponent);
+#endif
+        }
+
+        /// <summary>
+        /// Allows the game to run logic.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // tell the TimeRuler that we're starting a new frame. you always want
+            // to call this at the start of Update
+            this._timeRuler.StartFrame();
+
+            this._timeRuler.BeginMark("Update", Color.Blue);
+
+            // Update game components.
+            base.Update(gameTime);
+
+            this._timeRuler.EndMark("Update");
+        }
+
+        /// <summary>
+        /// Draws the scene.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            this._timeRuler.BeginMark("Draw", Color.Yellow);
+
+#if XNA
+            this._bloomComponent.BeginDraw();
+#endif
+
+            this.GraphicsDevice.Clear(Color.Black);
+
+            this.GraphicsDevice.RasterizerState = Engine.Instance.Rasterizer.State;
+
+            base.Draw(gameTime);
+
+            // Stop measuring time for "Draw".
+            this._timeRuler.EndMark("Draw");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+        }
+    }
+}
