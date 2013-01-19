7da619a67f47058808adbb911bd4677dbb203b03
Hüseyin Uslu <shalafiraistlin@gmail.com>
2013-01-20 01:12:08 +0200

    Fixing src/game's case.

---
+++ src/game.tmp/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Copyright (C) 2011-2012 Volumetric Studios
+ *
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("VoxeliqSample")]
+[assembly: AssemblyProduct("VoxeliqEngine")]
+[assembly: AssemblyDescription("A block engine.")]
+[assembly: AssemblyCompany("Voxeliq Studios")]
+[assembly: AssemblyCopyright("Copyright ©  2012 - 2013")]
+[assembly: AssemblyTrademark("Voxeliq Studios")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+
+[assembly: Guid("cfa55c08-9bd1-426b-b484-bc50e603b766")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+
+[assembly: AssemblyVersion("0.1.*")]
