030b57866da03dc8be73c2bd6c8de6e9a88baf51
Hüseyin Uslu <shalafiraistlin@gmail.com>
2013-10-20 02:06:31 +0300

    Pushing the base code for platform-manager based on http://www.int6.org/development/monogame/easy-multi-platform-managment-with-monogame/ Updated the copyright text.

--- src/Engine/Sky/NewSky.cs
+++ src/Engine/Sky/NewSky.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿/*
+ * Voxeliq Engine, Copyright (C) 2011 - 2013 Int6 Studios - All Rights Reserved. - http://www.int6.org - https://github.com/raistlinthewiz/voxeliq
+ *
+ * This file is part of Voxeliq Engine project. This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the Microsoft Public License (Ms-PL).
+ */
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Graphics.PackedVector;
 using VoxeliqEngine.Assets;
 using VoxeliqEngine.Blocks;
-using VoxeliqEngine.Chunks;
 using VoxeliqEngine.Common.Logging;
-using VoxeliqEngine.Common.Noise;
 using VoxeliqEngine.Graphics;
 using VoxeliqEngine.Graphics.Texture;
 using VoxeliqEngine.Universe;
