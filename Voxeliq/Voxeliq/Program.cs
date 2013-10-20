c24e10233f77b9571b8fe4fb9153cba2a7c41be6
Hüseyin Uslu <shalafiraistlin@gmail.com>
2013-10-21 01:10:42 +0300

    Pushing the skeleton console application.

---
+++ Voxeliq/Voxeliq/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Voxeliq Engine, Copyright (C) 2011 - 2013 Int6 Studios - All Rights Reserved. - https://github.com/raistlinthewiz/voxeliq
+ *
+ * This file is part of Voxeliq Engine project. This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the Microsoft Public License (Ms-PL).
+ */
+
+namespace Voxeliq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
